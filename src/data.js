b2b3f11166c6dfb20cd6e563e97f5dd22f02a536
Abel <abelmasila@gmail.com>
2020-01-13 17:55:27 +0300

    Added timelineData array

---
+++ src/data.js
@@ -0,0 +1,62 @@
+const timelineData = [
+  {
+    text: "Started working on the timeline component repository",
+    date: "Jan 13 2020",
+    category: {
+      tag: "timeline-repo",
+      color: "#FFDB14"
+    },
+    link: {
+      url: "https://github.com/abel-masila/timeline-component",
+      text: "Check it out on GitHub"
+    }
+  },
+  {
+    text: "Started the Weekly Coding Challenge program",
+    date: "March 04 2019",
+    category: {
+      tag: "blog",
+      color: "#e17b77"
+    },
+    link: {
+      url: "https://medium.com/@abel_masila",
+      text: "Check it out here"
+    }
+  },
+  {
+    text: "Got 1.000 followers on Twitter",
+    date: "March 07 2019",
+    category: {
+      tag: "twitter",
+      color: "#1DA1F2"
+    },
+    link: {
+      url: "https://twitter.com/abel_masila",
+      text: "See profile"
+    }
+  },
+  {
+    text: "I published my first article in the FreeCodeCamp Medium Publication",
+    date: "March 18 2019",
+    category: {
+      tag: "medium",
+      color: "#018f69"
+    },
+    link: {
+      url: "https://medium.com/@abel_masila",
+      text: "Check it out here"
+    }
+  },
+  {
+    text: "Over 12.000 views in a single day on my Medium posts",
+    date: "April 05 2019",
+    category: {
+      tag: "medium",
+      color: "#018f69"
+    },
+    link: {
+      url: "https://medium.com/@abel_masila",
+      text: "See profile"
+    }
+  }
+];
