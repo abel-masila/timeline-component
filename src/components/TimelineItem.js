8ca4219d6f6ca2545b9aa3659bbc8d18cc39187c
Abel <abelmasila@gmail.com>
2020-01-13 18:40:32 +0300

    All done

--- src/components/TimelineItem.js
+++ src/components/TimelineItem.js
@@ -6,7 +6,7 @@ const TimelineItem = ({ data }) => {
         <span className="tag" style={{ background: data.category.color }}>
           {data.category.tag}
         </span>
-        <time>{data.time}</time>
+        <time>{data.date}</time>
         <p>{data.text}</p>
         {data.link && (
           <a href={data.link.url} target="_blank" rel="noopener noreferrer">
