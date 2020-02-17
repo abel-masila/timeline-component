454954e35692ee5bc50aa4a83b4a2bda191d72e8
Abel <abelmasila@gmail.com>
2020-02-17 18:06:00 +0300

    updated test suite tittle

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders timeline component", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/React Timeline Component/i);
   expect(linkElement).toBeInTheDocument();
